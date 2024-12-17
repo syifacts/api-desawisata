c0473266c3a647eaa838d4e5d7ab046d3bb549e0
syifacts <ss4166269@gmail.com>
2024-12-17 20:39:28 +0700

    first commit

--- models/desawisata.js
+++ models/desawisata.js
@@ -1,21 +1,22 @@
 const mongoose = require('mongoose');
 
 const desaWisataSchema = new mongoose.Schema({
-    name: { type: String, required: true }, // Nama desa wisata
-    location: { type: String, required: true }, // Lokasi desa wisata
-    photo: { type: String, required: true }, // URL foto desa wisata
-    description: { type: String, required: true }, // Deskripsi desa wisata
-    longdesc: { type: String, required: true }, // Deskripsi panjang
-    urlvid: { type: String, required: true }, // URL video
-    reviews: [ // Array ulasan
-        {
-            reviewerName: { type: String, required: true }, // Nama pemberi ulasan
-            reviewText: { type: String, required: true }, // Teks ulasan
-            rating: { type: Number, min: 1, max: 5, required: true }, // Rating ulasan (1-5)
-            createdAt: { type: Date, default: Date.now } // Waktu ulasan ditambahkan
-        }
-    ]
-}, { timestamps: true });
+  name: String,
+  location: String,
+  description: String,
+  longdesc: String,
+  photo: String,
+  urlvid: String,
+  reviews: [
+    {
+      reviewerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // ID pengguna
+      reviewerName: { type: String, required: true }, // Nama pemberi ulasan
+      reviewText: { type: String, required: true }, // Teks ulasan
+      rating: { type: Number, min: 1, max: 5, required: true }, // Rating ulasan
+      createdAt: { type: Date, default: Date.now } // Waktu ulasan dibuat
+    }
+  ]
+});
 
 const DesaWisata = mongoose.model('DesaWisata', desaWisataSchema);
 
