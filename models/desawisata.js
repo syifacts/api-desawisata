662d9d7e138655412155b9488eafe7b484b01525
syifacts <ss4166269@gmail.com>
2024-12-17 22:52:43 +0700

    first commit

--- models/desawisata.js
+++ models/desawisata.js
@@ -1,17 +1,5 @@
 const mongoose = require('mongoose');
 
-const reviewSchema = new mongoose.Schema({
-    username: { type: String, required: true }, // Nama user yang mengirim ulasan
-    review: { type: String, required: true }, // Isi ulasan
-    rating: { 
-        type: Number, 
-        required: true, 
-        min: 1, 
-        max: 5 
-    }, // Rating antara 1 sampai 5
-    createdAt: { type: Date, default: Date.now }, // Waktu ulasan dibuat
-});
-
 const desaWisataSchema = new mongoose.Schema({
     name: { type: String, required: true }, // Nama desa wisata
     location: { type: String, required: true }, // Lokasi desa wisata
@@ -19,9 +7,9 @@ const desaWisataSchema = new mongoose.Schema({
     description: { type: String, required: true }, // Deskripsi desa wisata
     longdesc: { type: String, required: true }, // Deskripsi panjang
     urlvid: { type: String, required: true }, // URL video
-    reviews: [reviewSchema], // Array review
 }, { timestamps: true });
 
 const DesaWisata = mongoose.model('DesaWisata', desaWisataSchema);
 
 module.exports = DesaWisata;
+
