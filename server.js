915a52481ae9b6931bb9d3c2965e1cf06ba8a6ad
syifacts <ss4166269@gmail.com>
2024-12-17 11:50:47 +0700

    first commit

--- server.js
+++ server.js
@@ -4,51 +4,60 @@ require('dotenv').config();
 const agrowisataRoutes = require('./routes/routes');
 
 const startMongoDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URL, {
-        });
-        console.log('Connected to MongoDB');
-    } catch (err) {
-        console.error('MongoDB connection error:', err);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error('MongoDB connection error:', err.message);
+    process.exit(1);
+  }
 };
 
 const init = async () => {
-    const server = Hapi.server({
-        port: process.env.PORT || 3000,
-        host: '0.0.0.0',
-    });
+  const server = Hapi.server({
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0',
+  });
 
-// Register CORS middleware
-server.ext('onPreResponse', (request, h) => {
+  // Register CORS for all routes
+  server.ext('onPreResponse', (request, h) => {
     const response = request.response;
+    const headers = {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    };
 
     if (response.isBoom) {
-        // If the response is an error, we can still set CORS headers
-        response.output.headers['Access-Control-Allow-Origin'] = '*';
-        response.output.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE';
-        response.output.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization';
-    } else {
-        // For successful responses
-        response.headers['Access-Control-Allow-Origin'] = '*';
-        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE';
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization';
+      Object.assign(response.output.headers, headers);
+    } else if (response.headers) {
+      Object.assign(response.headers, headers);
     }
 
     return h.continue;
-});
+  });
 
-    // Register routes
-    server.route(agrowisataRoutes);
+  // Handle OPTIONS request for CORS preflight
+  server.route({
+    method: 'OPTIONS',
+    path: '/{any*}',
+    handler: (request, h) => h.response('CORS preflight response').code(200),
+  });
 
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
+  // Register routes
+  server.route(agrowisataRoutes);
+
+  // Start server
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
 };
 
-startMongoDB().then(() => {
-    init().catch(err => {
-        console.error('Server initialization error:', err);
-        process.exit(1);
-    });
-});
+startMongoDB()
+  .then(() => init())
+  .catch((err) => {
+    console.error('Server initialization error:', err.message);
+    process.exit(1);
+  });
